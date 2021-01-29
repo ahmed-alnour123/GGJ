9382864dee9a61c62390feebeadff6aa203b5375
ahmed alnour <ahmed2699@gmail.com>
2021-01-29 14:47:38 +0200

    made basic logic for player and world
    movment for player, and a sphere for the world and obstacles

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [Range(1, 100)]
+    public float speed;
+
+    void Update() {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        /// the we use x-axis to go forward and backward, and y-axis to rotate, we don't need z-axis
+        transform.Rotate(v * speed * Time.deltaTime, h * speed * Time.deltaTime, 0);
+    }
+}
