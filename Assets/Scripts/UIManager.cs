3e1490339a91ce00b88c6199814042c2418b40b5
abualmun <muntherhafiz474@gmail.com>
2021-01-30 18:58:02 +0200

    pause

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update  
+    public Text timer;
+    public float time = 300;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        time -= Time.deltaTime;
+        timer.text = ((int)time/60).ToString() + ":" + ((int)time%60).ToString() ;
+    }
+    public void WinGame()
+    {
+
+    }
+    public void LoseGame()
+    {
+
+    }
+}
