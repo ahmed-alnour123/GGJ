b4266f72e122df1f4f1f35274c2fd15f6a4f8470
abualmun <muntherhafiz474@gmail.com>
2021-01-30 08:41:34 +0200

    Barbarians movment and jump

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform player;
+
+    public Transform barbody;
+    public Transform playerbody;
+    [Range(1,50)]
+    public float speed;
+    public float jump_power,jump_distance,jump_counter,jump_cooldown;
+    public bool jumping;
+    public bool cooldown;
+    private float jump_start;
+    private Quaternion direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer(speed);
+        Jump(jump_power,jump_distance,jump_counter,jump_cooldown);
+    }
+
+    private void Jump(float jump_power,float jump_distance,float jump_counter,float jump_cooldown)
+    {
+        if(Time.time >= jump_start + jump_counter + jump_cooldown)
+                    cooldown = false;
+
+                    
+        if(Vector3.Distance(barbody.position,playerbody.position) < jump_distance && !cooldown)
+        {
+            if(!jumping) jump_start = Time.time;
+            jumping = true;
+            if(Time.time <= jump_start + jump_counter - 0.5f)
+            {
+                direction = player.rotation;
+            
+            }    else if(Time.time >= jump_start + jump_counter)
+                {
+
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation,direction,Time.deltaTime * jump_power);
+
+                if(Time.time >= jump_start + jump_counter + 0.3f)
+                    {
+                    cooldown = true;
+                    if(Time.time >= jump_start + jump_counter + jump_cooldown)
+                    cooldown = false;
+                    } 
+                }   
+
+        } else jumping = false;
+
+
+
+    }
+
+    private void FollowPlayer(float speed)
+    {
+        if(!cooldown)
+        transform.rotation = Quaternion.RotateTowards(transform.rotation,player.rotation,Time.deltaTime * speed);
+
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,12 +8,16 @@ public class Player : MonoBehaviour {
     public float verticalSpeed;
     [Range(1, 100)]
     public float horizontalSpeed;
+    public Transform barbarian;
 
     void Update() {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
         // the we use x-axis to go forward and backward, and y-axis to rotate, we don't need z-axis
-        transform.Rotate(v * verticalSpeed * Time.deltaTime, h * horizontalSpeed * Time.deltaTime, 0);
+        transform.Rotate(0, h * horizontalSpeed * Time.deltaTime, 0);
+        barbarian.Rotate(0, h * horizontalSpeed * Time.deltaTime, 0);
+        transform.Rotate(v * verticalSpeed * Time.deltaTime,0, 0);
     }
 }
+    
