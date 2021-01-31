285d339b5c94818d613f949bfae9c174ce0280f8
abualmun <muntherhafiz474@gmail.com>
2021-01-31 02:12:25 +0200

    ui semi finesh

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject levelsMenu;
+    public GameObject mainMenu;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadLevels()
+    {
+        levelsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Main");
+
+    }
+    public void Quit()
+    {
+
+    }
+    public void Return()
+    {
+        levelsMenu.SetActive(false);
+        mainMenu.SetActive(true); 
+    }
+}
