48ab0f573230de877962381095568fa3899ce0ea
ahmed alnour <ahmed2699@gmail.com>
2021-02-13 18:54:18 +0200

    changed player movement

---
+++ Assets/Scripts/Test_Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Movement : MonoBehaviour {
+
+    public float speed, rotSpeed;
+    private float h, v;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 newPos = transform.position + (transform.forward * v * speed * Time.deltaTime);
+        transform.Rotate(0, h * rotSpeed * Time.deltaTime, 0);
+
+        // fix position
+        Vector3 constrained = newPos.normalized * 25.5f;
+        transform.position = constrained;
+
+        // fix rotation
+        transform.rotation = Quaternion.FromToRotation(transform.up, transform.position.normalized) * transform.rotation;
+    }
+}
